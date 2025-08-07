7986efe9f7813135e984e253155a459a44c097d1
AKToran <aktoran60@gmail.com>
2025-08-07 23:07:50 +0600

    basic api endpoint and listen port

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = process.env || 3000;
+const port = process.env.PORT || 3000;
 
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("job portal recap is coooooking...");
+});
+
+app.listen(port, () => {
+  console.log("Job portal is running on port:", port);
+});
